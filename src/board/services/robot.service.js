14c58426941275659fe43d996fa4b88c51d7aa95
Paulo Soares <ps@nordsense.com>
2018-01-27 20:35:19 +0100

    iterate state multiple times

--- src/board/services/robot.service.js
+++ src/board/services/robot.service.js
@@ -19,14 +19,40 @@ class RobotService {
     * @param {string} actualPosition.y The y of the position
     * @param {string} actualPosition.direction The direction it's heading
     * @param {string} instruction L for left, R for right, F for forward
+    * @returns {Object} new position.
    */
   iterateState(actualPosition, instruction) {
     switch (instruction) {
       case 'L': return this.turnLeft(actualPosition);
       case 'R': return this.turnRight(actualPosition);
       case 'F': return this.moveForward(actualPosition);
-      default: throw new Error('Invalid instruction. Only L, R or F is allowed.')
+      default: throw new Error('Invalid instruction. Only L, R or F is allowed.');
     }
+  }
+
+  /**
+    * @param {Object} actualPosition The current position
+    * @param {string} actualPosition.x The x of the position
+    * @param {string} actualPosition.y The y of the position
+    * @param {string} actualPosition.direction The direction it's heading
+    * @param {string[]} instructions Multiple instructions L for left, R for right, F for forward
+    * @returns {Object[]} all the positions regarding the instructions.
+   */
+  multipleIterationOfState(actualPosition, instructions) {
+    return this.multipleIterationOfStateWithAcc(actualPosition, instructions, []);
+  }
+
+  multipleIterationOfStateWithAcc(actualPosition, instructions, accumulator) {
+    if (!instructions || !instructions.length) {
+      return accumulator;
+    }
+
+    const nextPosition = this.iterateState(actualPosition, instructions[0]);
+    return this.multipleIterationOfStateWithAcc(
+      nextPosition,
+      instructions.slice(1),
+      [...accumulator, nextPosition]
+    );
   }
 
   turnLeft(actualPosition) {
--- src/board/services/robot.service.test.js
+++ src/board/services/robot.service.test.js
@@ -1,6 +1,20 @@
 import { robotService } from './robot.service';
 
 describe('#iterateState', () => {
+  it('should throw an error for invalid instruction', () => {
+    expect(() => {
+      robotService.iterateState(
+        {
+          x: 3,
+          y: 3,
+          direction: 'S'
+        },
+        'X'
+      );
+    })
+      .toThrow(new Error('Invalid instruction. Only L, R or F is allowed.'));
+  })
+
   describe('with initial position 3,3,S', () => {
     let initialPosition;
 
@@ -106,6 +120,76 @@ describe('#iterateState', () => {
     it('with L it should go to 3,3,N', () => {
       expect(robotService.iterateState(initialPosition, 'L'))
         .toEqual({ x: 3, y: 3, direction: 'N' });
+    });
+  });
+});
+
+describe('#multipleIterationOfState', () => {
+  describe('with initial position 1,2,N', () => {
+    let initialPosition;
+
+    beforeEach(() => {
+      initialPosition = {
+        x: 1,
+        y: 2,
+        direction: 'N'
+      };
+    });
+
+    describe('with the instructions RFRFFRFRF', () => {
+      const instructions = 'RFRFFRFRF'.split('');
+      let result;
+
+      beforeEach(() => {
+        result = robotService.multipleIterationOfState(initialPosition, instructions);
+      });
+
+      it('should end in 1,3,N', () => {
+        expect(result[result.length - 1])
+          .toEqual({
+            x: 1,
+            y: 3,
+            direction: 'N'
+          })
+      });
+
+      it('should have 9 states in the response', () => {
+        expect(result.length).toBe(9);
+      });
+    });
+  });
+
+  describe('with initial position 1,2,N', () => {
+    let initialPosition;
+
+    beforeEach(() => {
+      initialPosition = {
+        x: 0,
+        y: 0,
+        direction: 'E'
+      };
+    });
+
+    describe('with the instructions RFLFFLRF', () => {
+      const instructions = 'RFLFFLRF'.split('');
+      let result;
+
+      beforeEach(() => {
+        result = robotService.multipleIterationOfState(initialPosition, instructions);
+      });
+
+      it('should end in 1,3,N', () => {
+        expect(result[result.length - 1])
+          .toEqual({
+            x: 3,
+            y: 1,
+            direction: 'E'
+          })
+      });
+
+      it('should have 8 states in the response', () => {
+        expect(result.length).toBe(9);
+      });
     });
   });
 });
